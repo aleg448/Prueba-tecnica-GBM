03a9f6cc63a2ca2c8c8762d2e995614106905795
aleg448 <90354487+aleg448@users.noreply.github.com>
2024-05-29 17:41:58 +0200

    Sol ejer 1

--- ejercicio 1/ejercicio 1.py
+++ ejercicio 1/ejercicio 1.py
@@ -1,7 +1,5 @@
-#input
-#clean text and whitespaces
-#reverse text
-#compare text and reverse
-#return "Es palindromo"
-#else return "No es palindromo"
-#yap
+#input, clean text and whitespaces, join into single continuous string
+P="".join(c.lower() for c in input("Ingresa texto a verificar:") if c.isalnum())
+#reverse text, compare text and reverse
+#return "Es palindromo",else return "No es palindromo"
+print("Es Palindromo" if P==P[::-1] else "No es Palindromo")
