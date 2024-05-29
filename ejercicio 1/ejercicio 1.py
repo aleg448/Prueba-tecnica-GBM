cb270dabc41dfc281aa2ea842eec10ed6426d835
aleg448 <90354487+aleg448@users.noreply.github.com>
2024-05-29 17:51:46 +0200

    Changed to function
    In order to be able to call from unit test

--- ejercicio 1/ejercicio 1.py
+++ ejercicio 1/ejercicio 1.py
@@ -1,5 +1,10 @@
-#input, clean text and whitespaces, join into single continuous string
-P="".join(c.lower() for c in input("Ingresa texto a verificar:") if c.isalnum())
-#reverse text, compare text and reverse
-#return "Es palindromo",else return "No es palindromo"
-print("Es Palindromo" if P==P[::-1] else "No es Palindromo")
+def es_palindromo():
+    P="".join(c.lower() for c in input("Ingresa texto a verificar:") if c.isalnum())
+    resultado="Es Palindromo" if P==P[::-1] else "No es Palindromo"
+    print(resultado)
+es_palindromo()
+
+"""
+input, clean text and whitespaces, join into single continuous string
+reverse text, compare text and reverse, return "Es palindromo",else return "No es palindromo"
+"""
